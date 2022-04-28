3b4d1845476a1146a00a9ce0bf6b6789800f80ba
Hassan-iqbal6 <43201426+Hassan-iqbal6@users.noreply.github.com>
2022-04-28 20:45:31 +0500

    cave cutscene updated

--- Assets/Scripts/CaveCutscene.cs
+++ Assets/Scripts/CaveCutscene.cs
@@ -12,22 +12,23 @@ public class CaveCutscene : MonoBehaviour
     [SerializeField]
     CinemachineVirtualCamera mCam;
     CinemachineTrackedDolly cam;
-  
+    FightController FC;
     GameObject player;
     private bool started;
     public bool last=false;
     string[] lines = {
-        "Watch out! micro tragedy!\n\n China wants to ban bitcoin again",
-        "Oh no Ethereum 2.0 is delayed 'till next year again",
-        "Be careful funds are not safu",
-        "Contract hacked. Funds drained",
-        "OMG another rug pull",
+       "Watch out! micro tragedy!\n\n China wants to ban bitcoin again",
+        "Oh no Ethereum 2.0\n\n is delayed \n\n 'till next year again",
+        "Be careful\n\n funds are not safu",
+        "Contract hacked.\n\n Funds drained",
+        "OMG\n\n another rug pull",
         "That meme coin is pumping.\n\n You should have bought",
-        "Aaaaaaaaaaaaaaand its gone"
+        "Aaaaaaaaaaaaaaand\n\n its gone..."
     };
     private void Start()
     {
         cam = mCam.GetCinemachineComponent<CinemachineTrackedDolly>();
+        FC = GameObject.FindGameObjectWithTag("Player").GetComponent<FightController>();
     }
 
     private void Update()
@@ -45,7 +46,6 @@ public class CaveCutscene : MonoBehaviour
                 mCam.Priority = 8;
                 this.enabled = false;
                 started = false;
-                var FC = player.GetComponent<FightController>();
                 FC.EnableMovement();
                 UIController.instance.DeactivateText();
             }
@@ -58,16 +58,21 @@ public class CaveCutscene : MonoBehaviour
             player = other.gameObject;
             var FC = player.GetComponent<FightController>();
             FC.DisableMovement();
-            if (FC.GetBearNumber() <= 7)
-                UIController.instance.ActivateText(lines[FC.GetBearNumber()].ToUpper());
-            else
-            {
-                int ind = Random.Range(0, 8);
-               UIController.instance.ActivateText(lines[ind].ToUpper());
-            }
+            Invoke("DisplayText", 1f);
             mCam.m_Priority = 20;
             started = true;
             transform.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+
+    void DisplayText()
+    {
+        if (FC.GetBearNumber() <= 7)
+            UIController.instance.ActivateText(lines[FC.GetBearNumber()].ToUpper());
+        else
+        {
+            int ind = Random.Range(0, 8);
+            UIController.instance.ActivateText(lines[ind].ToUpper());
         }
     }
 }
