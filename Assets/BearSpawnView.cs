5bacfdf30da15bb7f2b6d7a752b7bb5f9d9ba65d
Hassan-iqbal6 <43201426+Hassan-iqbal6@users.noreply.github.com>
2022-07-18 17:20:46 +0500

    added special bear on 5th bear

--- Assets/BearSpawnView.cs
+++ Assets/BearSpawnView.cs
@@ -28,21 +28,12 @@ public class BearSpawnView : MonoBehaviour
             case 3:
                 Instantiate(bears[3], transform).transform.localScale = Vector3.one * 2f; ;
                 break;
-            case 4:
-                Instantiate(bears[0], transform).transform.localScale = Vector3.one * 1.25f; ;
-                break;
-            case 5:
-                Instantiate(bears[1], transform).transform.localScale = Vector3.one * 1.5f; ;
-                break;
-            case 6:
-                Instantiate(bears[2], transform).transform.localScale = Vector3.one * 1.75f; ;
-                break;
-            case 7:
-                Instantiate(bears[3], transform).transform.localScale = Vector3.one * 2f; ;
-                break;
+
             default:
                 int randomNum = Random.Range(0, 4);
-                Instantiate(bears[randomNum], transform).transform.localScale = Vector3.one * 1.25f; ;
+                GameObject b = Instantiate(bears[randomNum], transform);
+                b.transform.localScale = Vector3.one * 2f;
+                b.transform.GetChild(2).gameObject.SetActive(true);
                 break;
         }
 
